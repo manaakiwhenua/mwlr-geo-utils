5517a4be7f71b5aad310826e385e39fdf9963a65
Ben Jolly <jollyb@landcareresearch.co.nz>
2022-08-23 04:34:06 +0000

    add setup.py, set appropriate execute flags

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='MWLR-Geo-Utils',
+      version='1.0',
+      description='Helpful utilities for working with geospatial data',
+      author='Ben Jolly',
+      author_email='bhjolly@gmail.com',
+      url='https://github.com/manaakiwhenua/mwlr-geo-utils',
+      scripts=[
+        'bin/mw-extent.py',
+        'bin/mw-gcptransform.py',
+        'bin/mw-gdalvalidate.py',
+        'bin/mw-hdfeosgetbands.py',
+        #'bin/mw-merge.py',
+        'bin/mw-rasterstats.py',
+        'bin/mw-rioscalc.py',
+        'bin/mw-setbanddescr.py',
+        'bin/mw-setvertband.sh',
+        ],
+     )
