4ea88ee63b8cccab985b939553bc9950909f0e8c
Ben Jolly <jollyb@landcareresearch.co.nz>
2022-08-24 13:21:41 +1200

    Updated mw-gcptransform.py
    Now provides more feedback to user
    Now auto-detects existing GCP SRS and uses that in the transform

--- bin/mw-gcptransform.py
+++ bin/mw-gcptransform.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 """Transform all GCPS in a raster to a different SRS (ie EPSG 4326 -> EPSG 3031)
+
+Usage:
+    mw-gcptransform.py my.tif 3031
+    OR
+    mw-gcptransform.py my.tif 3031 --output my_transformed.tif
 """
 # Author: Ben Jolly
 
@@ -25,19 +30,29 @@ def str_to_SRS(srs_str):
 
     return srs
 
-def transform_all_GCPs(raster, src_srs, tgt_srs):
+def transform_all_GCPs(raster, tgt_srs):
     """Read all GCPs from raster, transform them, the write them back"""
     ds = gdal.Open(raster, gdal.GA_Update)
-    transform = osr.CoordinateTransformation(src_srs, tgt_srs)
-    gcps = [transform_GCP(gcp, transform) for gcp in ds.GetGCPs()]
-    ds.SetGCPs(gcps, tgt_srs)
+    original_GCPs = ds.GetGCPs()
+    
+    if len(original_GCPs) == 0:
+        print("WARNING: No GCPs found in file, skipping:", raster)
+    else:
+        src_srs = osr.SpatialReference()
+        src_srs.ImportFromWkt(ds.GetGCPProjection())
+        if src_srs.IsSame(tgt_srs) == 1:
+            print("WARNING: New SRS is the same as the current GCP SRS, skipping:", raster)
+        else:
+            transform = osr.CoordinateTransformation(src_srs, tgt_srs)
+            transformed_GCPs = [transform_GCP(gcp, transform) for gcp in original_GCPs]
+            ds.SetGCPs(transformed_GCPs, tgt_srs)
+            print("Transformed GCPs in", raster)
 
     del ds
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Transform all GCPS in a raster to a different SRS (ie EPSG 4326 -> EPSG 3031)")
     parser.add_argument('raster')
-    parser.add_argument('s_srs', help="Source SRS (EPSG code, PROJ4 string, or WKT)")
     parser.add_argument('t_srs', help="Target SRS (EPSG code, PROJ4 string, or WKT)")
     parser.add_argument('--output', default=None, help="Copy raster and modify this one instead")
     args = parser.parse_args()
@@ -46,4 +61,4 @@ if __name__ == '__main__':
         shutil.copyfile(args.raster, args.output)
         args.raster = args.output
 
-    transform_all_GCPs(args.raster, str_to_SRS(args.s_srs), str_to_SRS(args.t_srs))
+    transform_all_GCPs(args.raster, str_to_SRS(args.t_srs))
