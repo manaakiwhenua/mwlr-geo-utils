76f6df9d0218c13ad3b18fb1aa57e23478d196b8
Ben Jolly <jollyb@landcareresearch.co.nz>
2024-02-26 09:53:30 +1300

    updates

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@
 from distutils.core import setup
 
 setup(name='mwlr-geo-utils',
-      version='1.0',
+      version='1.1',
       description='Helpful utilities for working with geospatial data',
       author='Ben Jolly',
       author_email='bhjolly@gmail.com',
       url='https://github.com/manaakiwhenua/mwlr-geo-utils',
       scripts=[
+        'bin/ext_to_poly.py',
         'bin/mw-extent.py',
         'bin/mw-gcptransform.py',
         'bin/mw-gdalvalidate.py',
         'bin/mw-hdfeosgetbands.py',
-        #'bin/mw-merge.py',
         'bin/mw-rasterstats.py',
         'bin/mw-rioscalc.py',
         'bin/mw-setbanddescr.py',
